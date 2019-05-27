7af8331ce84992ac6d6afaf1aea0aba28888573c
andre-frc <andre-frc@users.noreply.github.com>
2019-05-26 22:31:10 -0300

    Create Replace Temp With Query2.java

---
+++ Replace Temp With Query2.java
@@ -0,0 +1,95 @@
+public class Movie {
+
+  public static final int  CHILDRENS = 2;
+  public static final int  REGULAR = 0;
+  public static final int  NEW_RELEASE = 1;
+
+  private String _title;
+  private int _priceCode;
+
+  public Movie(String title, int priceCode) {
+      _title = title;
+      _priceCode = priceCode;
+  }
+
+  public int getPriceCode() {
+      return _priceCode;
+  }
+
+  public void setPriceCode(int arg) {
+     _priceCode = arg;
+  }
+
+  public String getTitle (){
+      return _title;
+  };
+}
+
+class Rental {
+    private Movie _movie;
+    private int _daysRented;
+
+    public Rental(Movie movie, int daysRented) {
+      _movie = movie;
+      _daysRented = daysRented;
+    }
+    public int getDaysRented() {
+      return _daysRented;
+    }
+    public Movie getMovie() {
+      return _movie;
+    }
+}
+
+class Customer {
+   private String _name;
+   private Vector _rentals = new Vector();
+
+   public Customer (String name){
+      _name = name;
+   };
+
+   public void addRental(Rental arg) {
+      _rentals.addElement(arg);
+   }
+   public String getName (){
+      return _name;
+   };
+  
+  public String statement() {
+   Enumeration rentals = _rentals.elements();
+   String result = "Rental Record for " + getName() + "\n";
+   while (rentals.hasMoreElements()) {
+      Rental each = (Rental) rentals.nextElement();
+
+      // show figures for this rental
+      result += "\t" + each.getMovie().getTitle()+ "\t" +
+                String.valueOf(each.getCharge()) + "\n";
+   }
+
+   // add footer lines
+   result +=  "Amount owed is " + String.valueOf(getTotalCharge()) + "\n";
+   result += "You earned " + String.valueOf(getTotalFrequentRenterPoints()) +
+                   " frequent renter points";
+   return result;
+}
+    
+    private double getTotalCharge() {
+       double result = 0;
+       Enumeration rentals = _rentals.elements();
+       while (rentals.hasMoreElements()) {
+          Rental each = (Rental) rentals.nextElement();
+          result += each.getCharge();
+       }
+       return result;
+     }
+
+     private int getTotalFrequentRenterPoints(){
+        int result = 0;
+        Enumeration rentals = _rentals.elements();
+        while (rentals.hasMoreElements()) {
+           Rental each = (Rental) rentals.nextElement();
+           result += each.getFrequentRenterPoints();
+        }
+        return result;
+     }
